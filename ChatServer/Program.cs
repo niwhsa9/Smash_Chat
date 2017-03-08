c721ac7b7ed2a825a6e2d3151f59023ce1e581a5
niwhsa9 <ashwingupta2000@gmail.com>
2017-03-08 08:38:30 -0800

    added framework for multi-client support

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -5,48 +5,75 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
+
 
 namespace ChatServer {
+
+    struct QueuedMessage {
+        public int clientID;
+        public string message;
+    }
+
     class Program {
-        static Socket handler = null;
-        static void send(string m) {
+        public static IPAddress ipAddress;
+        public static IPEndPoint serverEP;
+        static Socket[] handler = new Socket[10];
+        private static Thread[] client = new Thread[10];
+        public static int i = 0;
+        private static Thread listener;
+        public static QueuedMessage[] queue = new QueuedMessage[20];
+
+        static void send(string m, Socket handler) {
             byte[] msg = Encoding.ASCII.GetBytes(m);
             handler.Send(msg);
-            
         }
-  
-        static void Main(string[] args) {         
-            IPAddress ipAddress = IPAddress.Parse("10.0.0.8");
-            IPEndPoint serverEP = new IPEndPoint(ipAddress, 9191 );
 
-            Socket server = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
-            byte[] bytes = new Byte[1024];
-
+        static void listen(Socket hndl) {
             try {
-                server.Bind(serverEP);
-                server.Listen(10);
-                while (handler == null) {
-                    handler = server.Accept();
-                    //handler.Shutdown(SocketShutdown.Both);
-                    //handler.
-                }
-                send("Connected to " + ipAddress.ToString() + " on " + serverEP.Port);
-                while(true) {
-                     
-
+                i++;
+                while (true) {
+                   
                     byte[] data = new byte[1024];
                     Console.WriteLine("trying");
-                    int size = handler.Receive(data); //handler
+                    int size = hndl.Receive(data); //handler
 
                     string s = Encoding.ASCII.GetString(data, 0, size);
-                    send("echo " + s);
+                    send("echo " + s, hndl);
+
                 }
 
             } catch (Exception e) {
                 Console.WriteLine(e.ToString());
                 Console.WriteLine("Closing");
                 Console.ReadKey();
+            }
+        }
+
+        static void acceptConnections() {
+            ipAddress = IPAddress.Parse("10.0.0.8");
+            serverEP = new IPEndPoint(ipAddress, 9191);
+
+            Socket server = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(serverEP);
+            server.Listen(10);
+            while (true) {
+                handler[i] = server.Accept();
+                client[i] = new Thread(() => listen(handler[i]));
+                client[i].Start();
+                send("Connected to " + ipAddress.ToString() + " on " + serverEP.Port, handler[i]);
+               // i++;
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.
+            }
+        }
+
+        static void Main(string[] args) {
+            listener = new Thread(new ThreadStart(acceptConnections)); //new ThreadStart(listen)
+            listener.Start();
+            while(true) {
+            Console.WriteLine(i);
             }
         }
     }
